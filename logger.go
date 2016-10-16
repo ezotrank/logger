3ee6c5488f25e9b4d7b1f901b5cce3a734124d3b
Maxim Kremenev <ezo@kremenev.com>
2016-10-16 10:19:33 +0700

    change

--- logger.go
+++ logger.go
@@ -1,103 +1,61 @@
 package logger
 
 import (
-	"flag"
-	"log"
+	"io"
+	logger "log"
 	"os"
+	"os/signal"
+	"strings"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/hashicorp/logutils"
 )
 
 const (
-	FATALLV = iota
-	ERRORLV
-	WARNLV
-	INFOLV
-	DEBUGLV
-	VERBOSELV
+	DEFAULT_LOG_LEVEL = "INFO"
 )
 
-var logger Logger
+var (
+	Log *logger.Logger
+)
 
 func init() {
-	flag.IntVar(&logger.level, "v", 3, "log levels: Fatal,Error,Warn,Info,Debug,Verbose - from 0 to 5")
-	logger.init()
+	SetLogger(DEFAULT_LOG_LEVEL, "")
 }
 
-type Logger struct {
-	loggers map[uint]*log.Logger
-	level   int
-}
-
-func (self *Logger) init() {
-	self.loggers = map[uint]*log.Logger{
-		FATALLV:   log.New(os.Stderr, "fatal: ", log.Ldate|log.Ltime),
-		ERRORLV:   log.New(os.Stderr, "error: ", log.Ldate|log.Ltime),
-		WARNLV:    log.New(os.Stderr, "warning: ", log.Ldate|log.Ltime),
-		INFOLV:    log.New(os.Stderr, "info: ", log.Ldate|log.Ltime),
-		DEBUGLV:   log.New(os.Stderr, "debug: ", log.Ldate|log.Ltime),
-		VERBOSELV: log.New(os.Stderr, "verbose: ", log.Ldate|log.Ltime),
+func initLogFilter(logLevel string, logFile string) io.Writer {
+	writer := os.Stderr
+	if len(logFile) > 0 {
+		var err error
+		writer, err = os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+		if err != nil {
+			panic(err)
+		}
 	}
-}
-
-func AvailableForLevel(lvl int) bool {
-    return lvl <= logger.level
-}
-
-func (self *Logger) Println(level uint, v ...interface{}) {
-	if level <= uint(self.level) {
-		self.loggers[level].Println(v...)
+	filter := &logutils.LevelFilter{
+		Levels:   []logutils.LogLevel{"DEBUG", "INFO", "WARN", "ERROR", "NONE"},
+		MinLevel: logutils.LogLevel(logLevel),
+		Writer:   writer,
 	}
+	return filter
 }
 
-func (self *Logger) Printf(level uint, t string, v ...interface{}) {
-	if level <= uint(self.level) {
-		self.loggers[level].Printf(t, v...)
-	}
-}
-
-func Fatalln(v ...interface{}) {
-	logger.loggers[FATALLV].Fatalln(v...)
-}
-
-func Fatalf(tmpl string, v ...interface{}) {
-	logger.loggers[FATALLV].Fatalf(tmpl, v...)
-}
-
-func Infoln(v ...interface{}) {
-	logger.Println(INFOLV, v...)
-}
-
-func Infof(t string, v ...interface{}) {
-	logger.Printf(INFOLV, t, v...)
-}
-
-func Debugln(v ...interface{}) {
-	logger.Println(DEBUGLV, v...)
-}
-
-func Debugf(t string, v ...interface{}) {
-	logger.Printf(DEBUGLV, t, v...)
-}
-
-func Errorln(v ...interface{}) {
-	logger.Println(ERRORLV, v...)
-}
-
-func Errorf(t string, v ...interface{}) {
-	logger.Printf(ERRORLV, t, v...)
-}
-
-func Warnln(v ...interface{}) {
-	logger.Println(WARNLV, v...)
-}
-
-func Warnf(t string, v ...interface{}) {
-	logger.Printf(WARNLV, t, v...)
-}
-
-func Verboseln(v ...interface{}) {
-	logger.Println(VERBOSELV, v...)
-}
-
-func Verbosef(t string, v ...interface{}) {
-	logger.Printf(VERBOSELV, t, v...)
+func SetLogger(logLevel string, logFile string) {
+	logLevel = strings.ToUpper(logLevel)
+	mutex := &sync.Mutex{}
+	Log = &logger.Logger{}
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGUSR1)
+	go func() {
+		for sig := range c {
+			mutex.Lock()
+			Log.Printf("[WARN] Go a %v Signal! Reopen logs", sig)
+			Log.SetOutput(initLogFilter(logLevel, logFile))
+			mutex.Unlock()
+		}
+	}()
+	Log.SetFlags(logger.LstdFlags | logger.Lshortfile)
+	Log.SetOutput(initLogFilter(logLevel, logFile))
 }
